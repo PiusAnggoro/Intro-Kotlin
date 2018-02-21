1ebd43ea81e46c06de15d22dc22982799ef6f87c
piusanggoro <piusanggoro@gmail.com>
2018-02-21 14:22:31 +0700

    awal commit

---
+++ slide_config.js
@@ -0,0 +1,21 @@
+var SLIDE_CONFIG = {
+  settings: {
+    title: 'Pengenalan Kotlin',
+    subtitle: '',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    favIcon: 'images/kotlin-logo-tiny.png.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+  },
+  presenters: [{
+    name: 'Pius Anggoro',
+    company: 'STMIK AKAKOM Yogyakarta',
+    github: 'PiusAnggoro'
+  }]
+};
+
